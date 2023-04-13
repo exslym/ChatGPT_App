8a79f849c42348e85869e5bc6ff3bf3f67af410e
exslym <76844562+exslym@users.noreply.github.com>
2023-04-13 23:41:18 +0400

    api key updated and ignored from control version

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,5 +1,7 @@
+import '../env/api_key.dart';
+
 // ignore_for_file: non_constant_identifier_names
 String BASE_URL = "https://api.openai.com/v1";
 
-// String API_KEY = openAiApiKey;
-String API_KEY = "xxx";
+// create file lib/env/api_key.dart with String openApiKey = "your_openAI_API_KEY" taken from your openAI account
+String API_KEY = openApiKey;
