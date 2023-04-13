e69ec6db6019c9331b0c5ca60bc39831364e348a
exslym <76844562+exslym@users.noreply.github.com>
2023-04-14 00:56:05 +0400

    api keys enkrypt updated

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -3,5 +3,4 @@ import '../env/env.dart';
 // ignore_for_file: non_constant_identifier_names
 String BASE_URL = "https://api.openai.com/v1";
 
-// create file lib/env/api_key.dart with String openApiKey = "your_openAI_API_KEY" taken from your openAI account
 String API_KEY = Env.key;
